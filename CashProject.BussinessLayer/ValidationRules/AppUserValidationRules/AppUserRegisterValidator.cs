69bac4f633708cdb21018ad9349af7571daf13b7
Hasan Huseyin Yakut <hasanhuseyinyakut@gmail.com>
2024-08-15 22:05:08 +0300

    Fluent Validation

---
+++ CashProject.BussinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -0,0 +1,30 @@
+﻿using CashProject.DtoLayer.Dtos.AppUserDtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashProject.BussinessLayer.ValidationRules.AppUserValidationRules
+{
+    public class AppUserRegisterValidator : AbstractValidator<AppUserRegisterDto>
+    {
+        public AppUserRegisterValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Ad alanı boş geçilemez.");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Soyad alanı boş geçilemez");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email alanı boş geçilemez");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Şifre alanı boş geçilemez");
+            RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("Şifre tekrar alanı boş geçilemez");
+            RuleFor(x => x.Name).MaximumLength(30).WithMessage("Maximum 30 karakter olabilir");
+            RuleFor(x => x.Name).MinimumLength(2).WithMessage("En az 2 karakter olabilir");
+            RuleFor(y => y.ConfirmPassword).Equal(y => y.Password).WithMessage("Parolalar aynı değil");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Lütfen geçerli bir mail girin");
+
+            
+            
+            
+        }
+    }
+}
