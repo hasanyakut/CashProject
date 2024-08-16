02a89f13bcf9625b294634b2bcfaba03bbca4077
Hasan Huseyin Yakut <hasanhuseyinyakut@gmail.com>
2024-08-16 04:35:03 +0300

    Bussiness layer

---
+++ CashProject.BussinessLayer/Abstract/ICustomerAccountService.cs
@@ -0,0 +1,13 @@
+﻿using CashProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashProject.BussinessLayer.Abstract
+{
+    public interface ICustomerAccountService : IGenericService<CustomerAccount>
+    {
+    }
+}
---
+++ CashProject.BussinessLayer/Concrete/CustomerAccountProcessManager.cs
@@ -0,0 +1,46 @@
+﻿using CashProject.BussinessLayer.Abstract;
+using CashProject.DataAccessLayer.Abstract;
+using CashProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashProject.BussinessLayer.Concrete
+{
+    public class CustomerAccountProcessManager : ICustomerAccountProcessService
+    {
+        private readonly ICustomerAccountProcessDal _customerAccountProcessDal;
+
+        public CustomerAccountProcessManager(ICustomerAccountProcessDal customerAccountProcessDal)
+        {
+            _customerAccountProcessDal = customerAccountProcessDal;
+        }
+
+        public void TDelete(CustomerAccountProcess t)
+        {
+            _customerAccountProcessDal.Delete(t);
+        }
+
+        public CustomerAccountProcess TGetById(int id)
+        {
+            return _customerAccountProcessDal.GetById(id);
+        }
+
+        public List<CustomerAccountProcess> TGetList()
+        {
+            return _customerAccountProcessDal.GetList();
+        }
+
+        public void TInsert(CustomerAccountProcess t)
+        {
+            _customerAccountProcessDal.Insert(t);
+        }
+
+        public void TUpdate(CustomerAccountProcess t)
+        {
+            _customerAccountProcessDal.Update(t);
+        }
+    }
+}
